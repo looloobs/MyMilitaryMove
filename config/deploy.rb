c78d703cf731bdc13c82788559d0528efe17fd4d
looloobs <laurenrothlisberger@Macintosh.local>
2010-07-29 23:31:26 -0500

    db changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repository,  "git@github.com:looloobs/MyMilitaryMove.git"
 set :scm, "git"
 set :scm_verbose, true
 set :git_enable_submodules, 1
-set :scm_passphrase, "juYcxXE3a73y"
+
 
 # Uncomment this line if you're using SVN.  It makes deployments much faster
 set :deploy_via, :remote_cache
@@ -15,17 +15,17 @@ set :deploy_via, :remote_cache
 set :checkout, 'export'
 
 # The user you are using to deploy with (This user should have SSH access to your server)
-set :user, "afmove2"
+set :user, "deploy"
 
 # We want to deploy everything under your user, and we don't want to use sudo
 set :use_sudo, false
 
 # Where to deploy your application to.
-set :deploy_to, "/domains/militarymoveit.com/"
+set :deploy_to, "militarymoveit.com/"
 
 # -------------------------------- Server Definitions --------------------------------
 # Define the hostname of your server.  If you have multiple servers for multiple purposes, we can define those below as well.
-set :server_name, "sh01.blueboxgrid.com"
+set :server_name, "67.214.213.13"
 
 # We're assuming you're using a single server for your site, but if you have a seperate asset server or database server, you can specify that here.
 role :app, server_name
