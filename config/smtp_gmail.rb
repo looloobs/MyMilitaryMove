c667d96cf69f015aa0a98fecf44b46373f34ba9f
looloobs <laurenrothlisberger@Macintosh.local>
2010-08-06 11:25:44 -0500

    change smtp

--- config/smtp_gmail.rb
+++ config/smtp_gmail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
-  :authentication => :plain,
+  :authentication => :login,
   :domain => 'militarymoveit.com',
   :enable_starttls_auto => true,
   :user_name => "militarymoveit@gmail.com",
