1686ccd52294181c9a7aa29d8b0afc974a822df9
looloobs <laurenrothlisberger@Macintosh.local>
2010-08-06 13:53:03 -0500

    remove file

--- config/smtp_gmail.rb
+++
@@ -1,9 +0,0 @@
-
-ActionMailer::Base.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true,
-  :user_name => "militarymoveit@gmail.com",
-  :password => "lroth.101"
-}
