5c3d75c8b526806fa56a7e3101355edfc4e332f7
looloobs <laurenrothlisberger@Macintosh.local>
2010-08-06 12:18:23 -0500

    email update

--- config/environment.rb
+++ config/environment.rb
@@ -42,6 +42,17 @@ Rails::Initializer.run do |config|
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = { 
+    :tls => true,
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "militarymoveit.com", 
+    :user_name => "militarymoveit@gmail.com", 
+    :password => "lroth.101", 
+    :authentication => :login
+  }
+  
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
--- config/smtp_gmail.rb
+++ config/smtp_gmail.rb
@@ -1,8 +1,8 @@
+
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
-  :authentication => :login,
-  :domain => 'militarymoveit.com',
+  :authentication => :plain,
   :enable_starttls_auto => true,
   :user_name => "militarymoveit@gmail.com",
   :password => "lroth.101"
