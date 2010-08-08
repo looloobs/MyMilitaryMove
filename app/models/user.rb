44dd784a5532863b344fce6aa9a6adade0fbd9a1
looloobs <laurenrothlisberger@Macintosh.local>
2010-08-07 23:47:02 -0500

    change user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ActiveRecord::Base
   has_one :address
   has_one :spouse
   accepts_nested_attributes_for :pets, :families, :spouse, :address
+  
+  attr_accessible :name, :login, :email, :password
 
   def deliver_welcome_email!  
     UserMailer.deliver_welcome_email(self)  
