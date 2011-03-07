01a3747153e86efe901da5dfd032082678db2868
looloobs <laurenrothlisberger@Macintosh.local>
2011-03-07 09:18:42 -0600

    fixed error on contacts page

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110307031538) do
+ActiveRecord::Schema.define(:version => 20110307144758) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(:version => 20110307031538) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "like"
+    t.string   "email"
   end
 
   create_table "ends", :force => true do |t|
